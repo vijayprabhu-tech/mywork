88b3979359c160829994020c2cb11d43efeef149
vijayprabhu-tech <vij2277@gmail.com>
2026-06-27 19:28:03 +0530

    Delete ledgerbal directory

--- ledgerbal/src/main/java/com/bnk/ledger/Initializable.java
+++
@@ -1,8 +0,0 @@
-package com.bnk.ledger;
-
-
-@FunctionalInterface
-public interface Initializable {
-
-  void init(String className, ConnectionOptions options) throws Exception;
-}
--- ledgerbal/src/main/java/com/bnk/ledger/dao/ClientDao.java
+++
@@ -1,8 +0,0 @@
-package com.bnk.ledger.dao;
-
-import java.util.Date;
-
-public interface ClientDao {
-
-  void createClient(String clientRef, Date creationDate);
-}
