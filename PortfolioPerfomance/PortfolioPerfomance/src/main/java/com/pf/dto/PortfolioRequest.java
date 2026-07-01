0d492ae09175204616473520ca96216723b321fa
vijayprabhu-tech <vij2277@gmail.com>
2026-07-01 17:56:23 +0530

    initial Draft V1

---
+++ PortfolioPerfomance/PortfolioPerfomance/src/main/java/com/pf/dto/PortfolioRequest.java
@@ -0,0 +1,78 @@
+package main.java.com.pf.dto;
+
+
+public class PortfolioRequest {
+    private String portfolioId;
+    private String valuationDate;
+    private double beginMarketValue;
+    private double endMarketValue;
+    private double netCashFlow;
+    private double benchmarkReturnPct;
+    private String currency;
+    private String requestedBy;
+
+    public String getRequestedBy() {
+        return requestedBy;
+    }
+
+    public void setRequestedBy(String requestedBy) {
+        this.requestedBy = requestedBy;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getBenchmarkReturnPct() {
+        return benchmarkReturnPct;
+    }
+
+    public void setBenchmarkReturnPct(double benchmarkReturnPct) {
+        this.benchmarkReturnPct = benchmarkReturnPct;
+    }
+
+    public double getNetCashFlow() {
+        return netCashFlow;
+    }
+
+    public void setNetCashFlow(double netCashFlow) {
+        this.netCashFlow = netCashFlow;
+    }
+
+    public double getEndMarketValue() {
+        return endMarketValue;
+    }
+
+    public void setEndMarketValue(double endMarketValue) {
+        this.endMarketValue = endMarketValue;
+    }
+
+    public double getBeginMarketValue() {
+        return beginMarketValue;
+    }
+
+    public void setBeginMarketValue(double beginMarketValue) {
+        this.beginMarketValue = beginMarketValue;
+    }
+
+    public String getValuationDate() {
+        return valuationDate;
+    }
+
+    public void setValuationDate(String valuationDate) {
+        this.valuationDate = valuationDate;
+    }
+
+    public String getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(String portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+}
---
+++ PortfolioPerfomance/PortfolioPerfomance/src/main/java/com/pf/dto/PortfolioResponse.java
@@ -0,0 +1,81 @@
+package main.java.com.pf.dto;
+
+public class PortfolioResponse {
+
+    private String portfolioId;
+    private String valuationDate;
+    private double portfolioReturnPct;
+    private double benchmarkReturnPct;
+    private double exessReturnPct;
+    private String status;
+    private String[] reasons;
+    private String processedAt;
+
+    public String getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(String portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public String getValuationDate() {
+        return valuationDate;
+    }
+
+    public void setValuationDate(String valuationDate) {
+        this.valuationDate = valuationDate;
+    }
+
+    public double getPortfolioReturnPct() {
+        return portfolioReturnPct;
+    }
+
+    public void setPortfolioReturnPct(double portfolioReturnPct) {
+        this.portfolioReturnPct = portfolioReturnPct;
+    }
+
+    public double getBenchmarkReturnPct() {
+        return benchmarkReturnPct;
+    }
+
+    public void setBenchmarkReturnPct(double benchmarkReturnPct) {
+        this.benchmarkReturnPct = benchmarkReturnPct;
+    }
+
+    public double getExessReturnPct() {
+        return exessReturnPct;
+    }
+
+    public void setExessReturnPct(double exessReturnPct) {
+        this.exessReturnPct = exessReturnPct;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String[] getReasons() {
+        return reasons;
+    }
+
+    public void setReasons(String[] reasons) {
+        this.reasons = reasons;
+    }
+
+    public String getProcessedAt() {
+        return processedAt;
+    }
+
+    public void setProcessedAt(String processedAt) {
+        this.processedAt = processedAt;
+    }
+
+
+
+
+}
